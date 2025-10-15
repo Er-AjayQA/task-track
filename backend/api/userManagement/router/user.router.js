f33eb2a2b10c8013965aefbe493b7f39867f531a
Er-AjayQA <ajay.elitemindz@gmail.com>
2025-10-15 16:46:11 +0530

    User login signup all done

--- backend/api/userManagement/router/user.router.js
+++ backend/api/userManagement/router/user.router.js
@@ -2,16 +2,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller");
+const authMiddleware = require("../../../middleware/authMiddleware");
 
 // ***************** Routes ***************** //
 router.post(
   "/check-username-availability",
   userController.checkUsernameAvailability
 );
-router.post("/send-otp", userController.sendOtp);
+router.post("/send-otp", authMiddleware, userController.sendOtp);
+router.post("/resend-otp", userController.resendOtp);
+router.put("/otp-verification", authMiddleware, userController.otpVerification);
 router.post("/user-signup", userController.userSignup);
 router.post("/login-otp-verification", userController.loginOtpVerification);
-router.post("/signup-otp-verification", userController.signupOtpVerification);
+router.put("/signup-otp-verification", userController.signupOtpVerification);
 router.post("/login-with-password", userController.loginWithPassword);
 
 // ***************** Exports ***************** //
